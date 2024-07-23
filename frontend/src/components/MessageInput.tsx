6a1170e66dc918ddce70921bd91067ea0788af87
Breno RdV <noreply@raccoon.ninja>
2024-07-22 20:36:28 -0400

    added a loader for when we're waiting for the server to respond.

--- frontend/src/components/MessageInput.tsx
+++ frontend/src/components/MessageInput.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 import { FaPaperclip, FaPaperPlane, FaTimes } from 'react-icons/fa';
 import axios from 'axios';
-import {Message as MessageType, User, AiResponse, UploadDatafileResponse, ChatContextItem} from '../types/types';
+import { AiResponse, ChatContextItem, Message as MessageType, UploadDatafileResponse, User } from '../types/types';
 
 const InputContainer = styled.div`
     display: flex;
@@ -32,9 +32,10 @@ const FormContainer = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
+    position: relative;
 `;
 
-const TextArea = styled.textarea`
+const TextArea = styled.textarea<{ disabled: boolean }>`
     padding: 10px;
     font-size: 16px;
     border: 1px solid ${({ theme }) => theme.borderColor};
@@ -43,7 +44,55 @@ const TextArea = styled.textarea`
     color: ${({ theme }) => theme.color};
     resize: vertical; /* Allow vertical resizing */
     margin-bottom: 10px;
+    pointer-events: ${({ disabled }) => (disabled ? 'none' : 'auto')};
+    opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
 `;
+
+const MessageLoaderContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 0;
+
+    .loader {
+        margin-top: -50px;
+        width: 36px;
+        aspect-ratio: 1.154;
+        display: grid;
+        color: #000;
+        background: linear-gradient(to bottom left, #0000 calc(50% - 1px), currentColor 0 calc(50% + 1px), #0000 0) right/50% 100%,
+        linear-gradient(to bottom right, #0000 calc(50% - 1px), currentColor 0 calc(50% + 1px), #0000 0) left /50% 100%,
+        linear-gradient(currentColor 0 0) bottom/100% 2px;
+        background-repeat: no-repeat;
+        transform-origin: 50% 66%;
+        animation: l5 4s infinite linear;
+    }
+
+    .loader::before,
+    .loader::after {
+        content: "";
+        grid-area: 1/1;
+        background: inherit;
+        transform-origin: inherit;
+        animation: inherit;
+    }
+
+    .loader::after {
+        animation-duration: 2s;
+    }
+
+    @keyframes l5 {
+        100% {
+            transform: rotate(1turn);
+        }
+    }
+`;
+
+const messageLoader = (
+    <MessageLoaderContainer>
+        <div className="loader"></div>
+    </MessageLoaderContainer>
+);
 
 const ButtonContainer = styled.div`
     display: flex;
@@ -51,7 +100,7 @@ const ButtonContainer = styled.div`
     width: 100%;
 `;
 
-const Button = styled.button`
+const Button = styled.button<{ disabled: boolean }>`
     margin-left: 10px;
     padding: 10px;
     background-color: #0084ff;
@@ -61,6 +110,8 @@ const Button = styled.button`
     cursor: pointer;
     align-items: center;
     justify-content: center;
+    pointer-events: ${({ disabled }) => (disabled ? 'none' : 'auto')};
+    opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
 `;
 
 const PaperclipButton = styled(Button)`
@@ -92,6 +143,7 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSendMessage, currentUser 
     const [message, setMessage] = useState('');
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
     const [query, setQuery] = useState('');
+    const [loading, setLoading] = useState(false);
     const inputRef = useRef<HTMLTextAreaElement>(null);
     const fileInputRef = useRef<HTMLInputElement>(null);
 
@@ -103,6 +155,7 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSendMessage, currentUser 
     const handleSubmit = async (e?: React.FormEvent) => {
         e?.preventDefault();
         if (message.trim()) {
+            setLoading(true);
             let fileId: string | null = null;
 
             if (selectedFile && isDataAnalyst) {
@@ -115,6 +168,7 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSendMessage, currentUser 
                 } catch (error) {
                     console.error('File upload error:', error);
                     alert('Failed to upload file.');
+                    setLoading(false);
                     return;
                 }
             }
@@ -132,7 +186,7 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSendMessage, currentUser 
                     query: query,
                     file_id: fileId,
                     context,
-                }
+                };
             } else {
                 endpoint = '/ask-question';
                 payload = {
@@ -140,7 +194,7 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSendMessage, currentUser 
                     character_name: agentType,
                     context,
                     ruleset: process.env.REACT_APP_CHAT_RULESET,
-                }
+                };
             }
 
             const newMessage: MessageType = {
@@ -171,6 +225,8 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSendMessage, currentUser 
             } catch (error) {
                 console.error('Message sending error:', error);
                 alert('Failed to send message.');
+            } finally {
+                setLoading(false);
             }
 
             if (process.env.REACT_APP_DA_CLEAR_QUERY_AFTER_SEND === 'true') {
@@ -189,7 +245,7 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSendMessage, currentUser 
         if (!file) return false;
 
         return file.type === 'text/csv' || (file.type === 'application/vnd.ms-excel' && file.name.endsWith('.csv'));
-    }
+    };
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files && e.target.files.length > 0) {
@@ -233,6 +289,7 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSendMessage, currentUser 
                         value={query}
                         onChange={(e) => setQuery(e.target.value)}
                         placeholder="Query (not doing anything right now, but the backend is aware of it)"
+                        disabled={loading}
                     />
                 )}
                 <TextArea
@@ -242,24 +299,27 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSendMessage, currentUser 
                     onChange={(e) => setMessage(e.target.value)}
                     placeholder="Type a message..."
                     onKeyDown={handleKeyDown}
+                    disabled={loading}
                 />
                 <Hint>Press Ctrl+Enter to send</Hint>
                 <ButtonContainer>
                     {isDataAnalyst && (
-                        <PaperclipButton type="button" onClick={handlePaperclipClick}>
+                        <PaperclipButton type="button" onClick={handlePaperclipClick} disabled={loading}>
                             <FaPaperclip />
                         </PaperclipButton>
                     )}
-                    <SendButton type="submit">
+                    <SendButton type="submit" disabled={loading}>
                         <FaPaperPlane />
                     </SendButton>
                 </ButtonContainer>
+                {loading && messageLoader}
                 <input
                     type="file"
                     ref={fileInputRef}
                     style={{ display: 'none' }}
                     accept=".txt, .csv, .json"
                     onChange={handleFileChange}
+                    disabled={loading}
                 />
             </FormContainer>
         </InputContainer>
